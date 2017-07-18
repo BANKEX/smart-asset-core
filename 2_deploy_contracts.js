680142630681c67034003e4c00978f6dc886ae5f
Boris Kalinchuk <kalinchuk.borys@gmail.com>
2017-07-18 17:03:22 +0300

    Changing sensor simulation logic to be fired only from IotSimulation contract.

--- 2_deploy_contracts.js
+++ 2_deploy_contracts.js
@@ -1,5 +1,9 @@
 var SmartAsset = artifacts.require("./SmartAsset.sol");
+var IotSimulation = artifacts.require("./IotSimulation.sol");
 
 module.exports = function(deployer) {
-  deployer.deploy(SmartAsset);
+deployer.deploy(IotSimulation).then(function() {
+  return deployer.deploy(SmartAsset, IotSimulation.address);
+});
+
 };
