6996ff38f482b035e6a0566f40a558034788a5ed
gsamsonov <gsamsonov@dataart.com>
2017-07-19 16:25:11 +0300

    Fixes after code review

--- BankExCertifiedTest.js
+++ BankExCertifiedTest.js
@@ -2,6 +2,16 @@ var BankExCertified = artifacts.require("./BankExCertified.sol");
 
 contract('BankExCertified', function(accounts) {
 
+    it("Should return UnCertified by default", function() {
+
+        return BankExCertified.deployed().then(function(instance) {
+            return instance.isCertified.call("0xFF7766715a9Ea89007a2Fc6d2c2D7b6909490E25");
+
+        }).then(function(returnValue) {
+            assert.equal(returnValue, false);
+        })
+    });
+
     it("Should certify", function() {
         var bankExCertified;
 
@@ -29,12 +39,10 @@ contract('BankExCertified', function(accounts) {
     });
 
     it("Should throw on certify", function() {
-        var bankExCertified;
 
         return BankExCertified.deployed().then(function(instance) {
-            bankExCertified = instance;
 
-            return bankExCertified.certify("0xFF7766715a9Ea89007a2Fc6d2c2D7b6909490E25", {from : web3.eth.accounts[1]});
+            return instance.certify("0xFF7766715a9Ea89007a2Fc6d2c2D7b6909490E25", {from : web3.eth.accounts[1]});
 
         }).catch(function (error) {
             console.log('Expected error. Got it');
@@ -42,12 +50,10 @@ contract('BankExCertified', function(accounts) {
     });
 
     it("Should throw on unCertify", function() {
-        var bankExCertified;
 
         return BankExCertified.deployed().then(function(instance) {
-            bankExCertified = instance;
 
-            return bankExCertified.unCertify("0xFF7766715a9Ea89007a2Fc6d2c2D7b6909490E25", {from : web3.eth.accounts[1]});
+            return instance.unCertify("0xFF7766715a9Ea89007a2Fc6d2c2D7b6909490E25", {from : web3.eth.accounts[1]});
 
         }).catch(function (error) {
             console.log('Expected error. Got it');
