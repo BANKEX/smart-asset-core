a3fad87c08fc623886fac95a6b4097d9b5696c12
Boris Kalinchuk <kalinchuk.borys@gmail.com>
2017-07-25 16:50:51 +0300

    Adding test for SmartAssetPrice contract

--- SmartAssetPriceTest.js
+++ SmartAssetPriceTest.js
@@ -48,4 +48,73 @@ contract('IotSimulation', function(accounts) {
                  assert.equal(returnValue[7], 1, 'state should be PriceFromFormula1IsCalculated = position 1 in State enum list');
              });
      });
+
+     it("Check exception will be thrown in case asset is in step OnSale or after", function() {
+          var smartAssetGeneratedId;
+          var smartAsset;
+          var simulator;
+
+          SmartAsset.deployed().then(function(instance) {
+                  smartAsset = instance;
+                  return smartAsset.createAsset("BMW X5", "photo_url", "document_url");
+              }).then(function(result) {
+                  smartAssetGeneratedId = result.logs[0].args.id.c[0];
+                  return IotSimulation.deployed();
+              })
+              .then(function(instance) {
+                  simulator = instance;
+                  return simulator.setSmartAssetAddr(SmartAsset.address);
+              })
+              .then(function() {
+                  return SmartAssetPrice.deployed();
+              })
+              .then(function(instance) {
+                  smartAssetPrice = instance;
+                  return smartAssetPrice.setSmartAssetAddr(SmartAsset.address);
+              })
+              .then(function(result) {
+                  return simulator.generateIotOutput(smartAssetGeneratedId, 0);
+              })
+              .then(function(result) {
+                  return smartAsset.makeOnSale(smartAssetGeneratedId);
+              })
+              .then(function(result) {
+                  return simulator.generateIotOutput(smartAssetGeneratedId, 1);
+              })
+              .then(function(returnValue) {
+                  assert(false, "Throw was expected but didn't.");
+              }).catch(function(error) {
+                  console.log('Expected error. Got it');
+              });
+      });
+
+     it("Check exception will be thrown in case asset is in step OnSale or after", function() {
+          var smartAssetGeneratedId;
+          var smartAsset;
+          var simulator;
+
+          SmartAsset.deployed()
+              .then(function(result) {
+                  return IotSimulation.deployed();
+              })
+              .then(function(instance) {
+                  simulator = instance;
+                  return simulator.setSmartAssetAddr(SmartAsset.address);
+              })
+              .then(function() {
+                  return SmartAssetPrice.deployed();
+              })
+              .then(function(instance) {
+                  smartAssetPrice = instance;
+                  return smartAssetPrice.setSmartAssetAddr(SmartAsset.address);
+              })
+              .then(function(result) {
+                  return smartAssetPrice.checkSmartAssetModification(10000);
+              })
+              .then(function(returnValue) {
+                  assert(false, "Throw was expected but didn't.");
+              }).catch(function(error) {
+                  console.log('Expected error. Got it');
+              });
+      });
 });
