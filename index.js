8fd2bccbf5bfef7b62cda9ec7f2c29ab5eb256a4
Igor Chulinda <Igor.Chulinda@dataart.com>
2017-07-26 19:06:15 +0200

    Small ES6 styling improve

--- index.js
+++ index.js
@@ -1,11 +1,10 @@
 const TestRPC = require("ethereumjs-testrpc");
-const { spawn } = require('child_process')
+const { spawn } = require('child_process');
 const config = require('./truffle-config');
 
 TestRPC
     .server({
         logger: console
     })
-    .listen(config.networks.development, function (err, blockchain) {
-        spawn('npm', ['run', 'deploy'], { stdio: 'inherit' })
-    });
+    .listen(config.networks.development, (err, blockchain) =>
+        spawn('npm', ['run', 'deploy'], { stdio: 'inherit' }));
