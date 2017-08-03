59cf5b1ec063962665457141dfc82959bc7e4e76
gsamsonov <gleb.samsonov@dataart.com>
2017-08-03 13:08:21 +0300

    BuySmartAsset changes for new design

--- contracts/BuySmartAsset.sol
+++ contracts/BuySmartAsset.sol
@@ -5,44 +5,34 @@ pragma solidity ^0.4.10;
  * Interface for SmartAsset contract
  */
 contract SmartAssetInterface {
-	function sellAsset(uint id, address newOwner);
-	function getAssetOwnerById(uint id) constant
-    returns (address);
+    function sellAsset(uint id, address newOwner);
+    function getAssetOwnerById(uint id) constant returns (address);
+    function getAssetTypeById(uint id) constant returns (bytes32);
 }
 
-/**
- * Interface for SmartAssetPrice contract
- */
-contract SmartAssetPriceInterface {
-	function getSmartAssetPrice(uint id) constant returns (uint price);
-	function checkSmartAssetModification(uint assetId) constant returns (bool modified);
+
+contract BaseSmartAssetLogic {
+    function getSmartAssetPrice(uint id) returns(uint);
+    function checkSmartAssetModification(uint id) returns (bool);
+    function calculateDeliveryPrice(uint id, bytes32 city) returns(uint);
+    function getSmartAssetAvailability(uint id) returns (bool);
 }
 
-/**
- * Interface for SmartAssetAvailability contract
- */
-contract SmartAssetAvailabilityInterface {
-	function getSmartAssetAvailability(uint id) constant returns (bool availability);
+
+contract SmartAssetMetaInterface {
+    function getAssetLogicAddress(bytes32 assetType) constant returns(address);
 }
 
-/**
- * Interface for DeliveryRequirements contract
- */
-contract DeliveryRequirementsInterface {
-	function calculatePrice(uint id, bytes32 cityName) constant returns(uint);
-}
 
 /**
  * @title Buy smart asset contract
  */
 contract BuySmartAsset {
-	address public owner = msg.sender;
-	address private smartAssetAddr;
-	address private smartAssetPriceAddr;
-	address private smartAssetAvailabilityAddr;
-	address private deliveryRequirementsAddr;
+    address public owner = msg.sender;
+    address private smartAssetAddr;
+    address private smartAssetMetaAddr;
 
-	/**
+    /**
      * Check whether contract owner executes method or not
      */
     modifier onlyOwner {
@@ -56,46 +46,45 @@ contract BuySmartAsset {
      * @param deliveryRequirementsAddress Address of deployed DeliveryRequirementsAddress contract
      * @param smartAssetAddress Address of deployed SmartAssetAddress contract
      */
-    function BuySmartAsset(address smartAssetPriceAddress, address smartAssetAvailabilityAddress, address deliveryRequirementsAddress, address smartAssetAddress) {
-    	if (smartAssetPriceAddress == address(0) || smartAssetAvailabilityAddress == address(0) || deliveryRequirementsAddress == address(0) || smartAssetAddress == address(0)) {
+    function BuySmartAsset(address smartAssetAddress, address _smartAssetMetaAddr) {
+        if (smartAssetAddress == address(0) || _smartAssetMetaAddr == address(0)) {
             throw;
-        } 
+        }
 
-    	smartAssetAddr = smartAssetAddress;
-		smartAssetPriceAddr = smartAssetPriceAddress;
-		smartAssetAvailabilityAddr = smartAssetAvailabilityAddress;
-		deliveryRequirementsAddr = deliveryRequirementsAddress;
-	}
+        smartAssetMetaAddr = _smartAssetMetaAddr;
+        smartAssetAddr = smartAssetAddress;
+    }
 
     /**
      * @dev Returns total price of the asset
      * @param assetId Id of smart asset
      * @param cityName City name of destination/delivery city
      */
-	function getTotalPrice(uint assetId, bytes32 cityName) constant returns (uint totalPrice) {
-		SmartAssetPriceInterface smartAssetPriceInterface = SmartAssetPriceInterface(smartAssetPriceAddr);
+    function getTotalPrice(uint assetId, bytes32 cityName) constant returns (uint totalPrice) {
 
-		if (!smartAssetPriceInterface.checkSmartAssetModification(assetId)) {
-			// Formula1 parameters were changed/mutated
-			throw;
-		}
+        BaseSmartAssetLogic baseSmartAssetLogic = getBaseAssetLogic(assetId);
 
-		DeliveryRequirementsInterface deliveryRequirementsInterface = DeliveryRequirementsInterface(deliveryRequirementsAddr);
 
-		return smartAssetPriceInterface.getSmartAssetPrice(assetId) + deliveryRequirementsInterface.calculatePrice(assetId, cityName);
-	}
+        if (!baseSmartAssetLogic.checkSmartAssetModification(assetId)) {
+            // Formula1 parameters were changed/mutated
+            throw;
+        }
+
+        return baseSmartAssetLogic.getSmartAssetPrice(assetId) + baseSmartAssetLogic.calculateDeliveryPrice(assetId, cityName);
+    }
 
     /**
      * @dev Performs buying of the asset
      * @param assetId Id of smart asset
      * @param cityName City name of destination/delivery city
      */
-	function buyAsset(uint assetId, bytes32 cityName) payable {		
-		SmartAssetAvailabilityInterface SmartAssetAvailability = SmartAssetAvailabilityInterface(smartAssetAvailabilityAddr);
-		if (!SmartAssetAvailability.getSmartAssetAvailability(assetId)) {
-			// Asset is not avaiable via IoT sensor
-			throw;
-		}
+    function buyAsset(uint assetId, bytes32 cityName) payable {
+
+        BaseSmartAssetLogic baseSmartAssetLogic = getBaseAssetLogic(assetId);
+
+        if (!baseSmartAssetLogic.getSmartAssetAvailability(assetId)) {
+            throw;
+        }
 
 		uint totalPrice = getTotalPrice(assetId, cityName);
 
@@ -110,6 +99,21 @@ contract BuySmartAsset {
 		// Refund buyer if overpaid
 		msg.sender.transfer(msg.value - totalPrice);
 
-		smartAssetInterface.sellAsset(assetId, msg.sender);
-	}
+        smartAssetInterface.sellAsset(assetId, msg.sender);
+    }
+
+    function getBaseAssetLogic(uint assetId) private returns(BaseSmartAssetLogic) {
+
+        SmartAssetInterface smartAssetInterface = SmartAssetInterface(smartAssetAddr);
+
+        bytes32 assetType = smartAssetInterface.getAssetTypeById(assetId);
+
+        SmartAssetMetaInterface smartAssetMetaInterface = SmartAssetMetaInterface(smartAssetMetaAddr);
+
+        address assetLogicAddress = smartAssetMetaInterface.getAssetLogicAddress(assetType);
+
+        BaseSmartAssetLogic baseSmartAssetLogic = BaseSmartAssetLogic(assetLogicAddress);
+
+        return baseSmartAssetLogic;
+    }
 }
